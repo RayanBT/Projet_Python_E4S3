c4db278557943c43dca992af25e9ff4268779549
Elise <chabrerie.elise@gmail.com>
2025-10-22 11:22:50 +0200

    Optimisation du clean des data

--- src/utils/clean_data.py
+++ src/utils/clean_data.py
@@ -1,25 +1,46 @@
 import pandas as pd
 
-# Nom des fichiers csv
 input_file = "data/raw/effectifs.csv"
 output_file = "data/clean/csv_clean.csv"
 
-# Définir les colonnes obligatoires
+# Colonnes à conserver
 cols_required = [
     "annee", "patho_niv1", "top", "cla_age_5", "sexe", "region", "dept",
     "Ntop", "Npop", "prev", "Niveau prioritaire", "libelle_sexe", "tri"
 ]
 
-# Charger le fichier CSV
-df = pd.read_csv(input_file, sep=";")
+# Optionnel : déclarer les types si connus
+dtypes = {
+    "annee": "Int32",  # Nullable integer
+    "top": "category",
+    "sexe": "category",
+    "region": "category",
+    "dept": "category",
+    "Ntop": "float32",
+    "Npop": "float32",
+    "prev": "float32",
+    "Niveau prioritaire": "category",
+    "libelle_sexe": "category",
+    "tri": "float32"
+}
 
-# Supprimer les lignes avec des valeurs vides dans les colonnes obligatoires
-df_clean = df.dropna(subset=cols_required, how="any")
-# Supprimer les lignes où une des colonnes obligatoires contient une chaîne vide
-df_clean = df_clean[~df_clean[cols_required].apply(lambda x: x.eq("").any(), axis=1)]
+# Charger uniquement les colonnes utiles (plus rapide)
+df = pd.read_csv(
+    input_file,
+    sep=";",
+    usecols=lambda col: col in cols_required, #ignore les colonnes inutiles
+    dtype=dtypes,
+    na_values=[""]  # les chaînes vides seront considérées comme NaN
+)
 
-# Supprimer la colonne 'libelle_classe_age'
-df_clean = df_clean.drop(columns=["libelle_classe_age"], errors="ignore")
+# Afficher le nombre de lignes initial
+print(f"Nombre de lignes initiales : {len(df)}")
 
-# Enregistrer le fichier nettoyé
+# Supprimer les lignes avec NaN 
+df_clean = df.dropna(subset=cols_required)
+
+# Afficher le nombre de lignes après nettoyage
+print(f"Nombre de lignes après nettoyage : {len(df_clean)}")
+
+# Export
 df_clean.to_csv(output_file, index=False, encoding="utf-8")
